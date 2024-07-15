5a9e74cd4449b2a756611f98b9ec18943801d812
CoderOO7 <shubhamyadav.70007@outlook.com>
2024-07-15 22:47:32 +0530

    feat: flaky unit test examples

---
+++ server/svc/kreditcart-product-catalogue-svc/src/test/java/ConcurrencyTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.css.Counter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConcurrencyTest {
+    private int count = 1;
+
+    void increamentCount() {
+        this.count = this.count + 1;
+    }
+
+    @Test
+    public void testConcurrency() {
+        Thread t1 = new Thread(() -> increamentCount());
+        Thread t2 = new Thread(() -> increamentCount());
+
+        t1.start();
+        t2.start();
+
+        assertEquals(2, this.count);
+    }
+}
