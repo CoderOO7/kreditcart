71e6d4b4636d308bb3591c306eb2f8cdbeb5984d
CoderOO7 <shubhamyadav.70007@outlook.com>
2024-07-16 20:47:01 +0530

    feat: implement productMVC unit test

---
+++ server/svc/kreditcart-product-catalogue-svc/src/test/java/com/kreditcart/productCatalogue/Controllers/ProductControllerTest.java
@@ -0,0 +1,61 @@
+package com.kreditcart.productCatalogue.Controllers;
+
+import com.kreditcart.productCatalogue.Models.Product;
+import com.kreditcart.productCatalogue.Services.IProductService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class ProductControllerTest {
+    @Autowired
+    private ProductController productController;
+
+//    @Autowired
+    @MockBean  // it will instruct spring that this productService is mocked, and pass it wherever it's needed
+    private IProductService productService;
+
+
+    @Test
+    @DisplayName("GetProduct")
+    public void Test_GetProduct_ReturnProduct() {
+        Product product = new Product();
+        product.setTitle("Thar 5 Door");
+        product.setPrice((double)2000000);
+
+        // Arrange
+        when(productService.getProduct(anyLong())).thenReturn(product);
+
+        // Act
+        ResponseEntity<Product> productResp = productController.getProduct(1L);
+
+        // assert
+        assertNotNull(productResp.getBody());
+        assertEquals(2000000, productResp.getBody().getPrice());
+        assertEquals("Thar 5 Door", productResp.getBody().getTitle());
+    }
+
+    @Test
+    @DisplayName("GetProductInternalDependency")
+    public void Test_GetProduct_InternalDependencyThrowsException() {
+
+        // arrange
+        when(productService.getProduct(anyLong())).thenThrow(new RuntimeException("Something went very wrong"));
+
+        // act and assert
+        assertThrows(RuntimeException.class, ()->productController.getProduct(1L));
+    }
+
+    @Test
+    @DisplayName("GetProductWithInvalidId")
+    public void Test_GetProductWithInvalidId_ThrowsException() {
+        assertThrows(RuntimeException.class, ()->productController.getProduct(0L));
+    }
+}
