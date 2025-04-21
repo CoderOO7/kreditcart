495366a55d4ff045262a689d848f7ffaed76e434
CoderOO7 <shubhamyadav.70007@outlook.com>
2025-04-21 16:29:15 +0530

    chore(inventory-svc): setup Flyway and add initial migration

--- server/svc/kreditcart-inventory-svc/src/main/resources/db/migration/V1__create_inventory_table.sql
+++ server/svc/kreditcart-inventory-svc/src/main/resources/db/migration/V1__create_inventory_table.sql
@@ -1,10 +1,13 @@
-CREATE TABLE inventory (
-  id BIGINT NOT NULL,
-   created_at datetime NOT NULL,
-   updated_at datetime NOT NULL,
-   product_id BIGINT NOT NULL,
-   quantity INT NOT NULL,
-   CONSTRAINT pk_inventory PRIMARY KEY (id)
-);
+-- Enable UUID extension
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA public;
 
-ALTER TABLE inventory ADD CONSTRAINT uc_inventory_productid UNIQUE (product_id);
+CREATE TABLE inventories (
+   id UUID DEFAULT uuid_generate_v4() NOT NULL,
+   is_active BOOLEAN DEFAULT TRUE NOT NULL,
+   created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+   updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+   product_id UUID NOT NULL,
+   quantity INTEGER DEFAULT 0 NOT NULL,
+   CONSTRAINT pk_inventories PRIMARY KEY (id)
+);
+ALTER TABLE inventories ADD CONSTRAINT uc_inventories_productid UNIQUE (product_id);
