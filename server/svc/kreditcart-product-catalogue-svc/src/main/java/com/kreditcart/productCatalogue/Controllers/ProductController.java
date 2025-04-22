0a98ddfecf109d9ffa9eb5dc4ad4897fb179996f
CoderOO7 <shubhamyadav.70007@outlook.com>
2025-04-22 19:17:24 +0530

    feat(product-svc): api versioning

--- server/svc/kreditcart-product-catalogue-svc/src/main/java/com/kreditcart/productCatalogue/Controllers/ProductController.java
+++ server/svc/kreditcart-product-catalogue-svc/src/main/java/com/kreditcart/productCatalogue/Controllers/ProductController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-@RequestMapping("/products")
+@RequestMapping("/api/v1/product-svc/products")
 @RestController
 public class ProductController {
 
